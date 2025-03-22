d717af71f4846999b4b09ef35de8377bc2e3339e
vladimir <vovka1998@gmail.com>
2025-03-22 19:58:36 +0300

    added models Tag, Task

--- todo_list/tasks_todo/models.py
+++ todo_list/tasks_todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from todo_list.core.models import Profile
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Тег")
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовое задачи")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    description = models.TextField(max_length=350, verbose_name="Описание задачи")
+    date_end = models.DateTimeField(verbose_name="Дата исполнения")
+
+    tags = models.ManyToManyField(Tag, related_name="tags", verbose_name="Теги")
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+        verbose_name="Профиль пользователя"
+    )
