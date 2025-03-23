fa42c19d5ce3f57005383b4591566abfa4b21464
vladimir <vovka1998@gmail.com>
2025-03-23 10:05:23 +0300

    added PK

--- todo_list/core/models.py
+++ todo_list/core/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class ProfileUser(models.Model):
     bio = models.TextField(max_length=250, verbose_name="О себе")
-    telegram_id = models.IntegerField(verbose_name="ID пользователя в телеграмме")
+    tg_id = models.IntegerField(primary_key=True, verbose_name="ID пользователя в телеграмме")
