868d3538a3959b81aa03fa4767eec359237ac3e4
vladimir <vovka1998@gmail.com>
2025-03-24 14:10:52 +0300

    deleted app core

--- todo_list/core/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-
-class ProfileUser(models.Model):
-    bio = models.TextField(max_length=250, verbose_name="О себе")
-    tg_id = models.IntegerField(primary_key=True, verbose_name="ID пользователя в телеграмме")
