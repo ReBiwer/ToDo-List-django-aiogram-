70045ea4b92a9870df5138dee1d82d0afe90f1a6
vladimir <vovka1998@gmail.com>
2025-03-24 14:29:46 +0300

    fixed verbose_name

--- todo_list/tasks_todo/models.py
+++ todo_list/tasks_todo/models.py
@@ -13,7 +13,7 @@ class Tag(models.Model):
 class Task(models.Model):
     pk = models.CharField(primary_key=True)
     tg_id = models.IntegerField(verbose_name="ID пользователя в телеграмме")
-    title = models.CharField(max_length=200, verbose_name="Заголовое задачи")
+    title = models.CharField(max_length=200, verbose_name="Заголовок задачи")
     created_at = models.DateTimeField(default=timezone.now, verbose_name="Время создания")
     description = models.TextField(max_length=350, verbose_name="Описание задачи")
     date_end = models.DateTimeField(verbose_name="Дата исполнения")
