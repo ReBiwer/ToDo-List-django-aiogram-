37a9f44bdb4bf8385ec7550e11835913689f2f50
vladimir <vovka1998@gmail.com>
2025-03-24 21:20:12 +0300

    added API for Tag

--- todo_list/tasks_todo/views.py
+++ todo_list/tasks_todo/views.py
@@ -1,9 +1,15 @@
 from adrf.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
-from tasks_todo.models import Task
-from tasks_todo.serializers import TaskSerializer
+from tasks_todo.models import Task, Tag
+from tasks_todo.serializers import TaskSerializer, TagSerializer
 
 
 class TaskAPI(ModelViewSet):
     queryset = Task.objects.prefetch_related("tags").all()
     serializer_class = TaskSerializer
+
+class TagAPI(ModelViewSet):
+    queryset = Tag.objects.prefetch_related("tasks").all()
+    serializer_class = TagSerializer
+    lookup_field = "pk_tag"
