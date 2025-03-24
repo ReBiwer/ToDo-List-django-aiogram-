afadb1703b965e98129f82d6ad379369237bda46
vladimir <vovka1998@gmail.com>
2025-03-24 19:52:48 +0300

    connected signal

--- todo_list/tasks_todo/apps.py
+++ todo_list/tasks_todo/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TasksTodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks_todo'
+
+    def ready(self):
+        import tasks_todo.signals
