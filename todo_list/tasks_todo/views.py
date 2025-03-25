2b17babde080897ea2482ae7ca4d49df9407d288
vladimir <vovka1998@gmail.com>
2025-03-25 12:22:21 +0300

    updated alist method TaskAPI

--- todo_list/tasks_todo/views.py
+++ todo_list/tasks_todo/views.py
@@ -9,7 +9,35 @@ class TaskAPI(ModelViewSet):
     queryset = Task.objects.prefetch_related("tags").all()
     serializer_class = TaskSerializer
 
+
+    @extend_schema(operation_id="task_v1_aretrive")
+    async def aretrieve(self, request, *args, **kwargs):
+        await super().aretrieve(request, *args, **kwargs)
+
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name='tg_id',
+                location=OpenApiParameter.QUERY,
+                description='ID пользователя в телеграмм',
+                required=True,
+                type=int
+            ),
+        ]
+    )
+    async def alist(self, tg_id: int, *args, **kwargs):
+        tg_id = self.request.query_params["tg_id"]
+        self.queryset = Task.objects.filter(tg_id=tg_id).all()
+        return await super().alist(*args, **kwargs)
+
+
 class TagAPI(ModelViewSet):
     queryset = Tag.objects.prefetch_related("tasks").all()
     serializer_class = TagSerializer
     lookup_field = "pk_tag"
+
+
+    @extend_schema(operation_id="tag_v1_aretrive")
+    async def aretrieve(self, request, *args, **kwargs):
+        await super().aretrieve(request, *args, **kwargs)
