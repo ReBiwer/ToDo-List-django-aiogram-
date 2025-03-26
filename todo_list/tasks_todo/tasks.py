4d650d1e69665f3448dd9800e90ffad404bf2292
vladimir <vovka1998@gmail.com>
2025-03-26 19:57:21 +0300

    fixed task for notification

--- todo_list/tasks_todo/tasks.py
+++ todo_list/tasks_todo/tasks.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+import asyncio
 
 from aiogram import Bot
 from celery import shared_task
@@ -9,4 +9,4 @@ from todo_list.settings import TELEGRAM_BOT_TOKEN
 @shared_task(name="Уведомление о выполнении задачи")
 def notification_start_task(msg: str, tg_id: int):
     bot = Bot(token=TELEGRAM_BOT_TOKEN)
-    bot.send_message(tg_id, msg)
+    asyncio.run(bot.send_message(tg_id, msg))
