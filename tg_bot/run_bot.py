58854d6299cee0742b2a2d3e5a803b172f4cdad1
vladimir <vovka1998@gmail.com>
2025-03-26 09:38:06 +0300

    deleted RedisStorage

--- tg_bot/run_bot.py
+++ tg_bot/run_bot.py
@@ -2,24 +2,18 @@ import dotenv
 import os
 import logging
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from aiogram_dialog import setup_dialogs
 from routers.start import start_router, start_dialog
 
 dotenv.load_dotenv()
 
-REDIS_HOST = os.environ.get("REDIS_HOST")
-REDIS_PORT = os.environ.get("REDIS_PORT")
-REDIS_NUM_DB = os.environ.get("REDIS_NUM_DB_TG")
-
-redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_NUM_DB}"
-
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 
-storage = RedisStorage.from_url(redis_url)
+storage = MemoryStorage()
 bot = Bot(token=os.environ.get("TELEGRAM_BOT_TOKEN"))
 dp = Dispatcher(storage=storage)
 dp.include_router(start_dialog)
