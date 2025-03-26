5f8ac464c4541f5c0e821f8c04bdba8981d6a1da
vladimir <vovka1998@gmail.com>
2025-03-26 09:48:10 +0300

    registered models in admin

--- todo_list/tasks_todo/admin.py
+++ todo_list/tasks_todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task, Tag
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["pk_task", "tg_id", "title", "created_at"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["pk_tag", "name"]
