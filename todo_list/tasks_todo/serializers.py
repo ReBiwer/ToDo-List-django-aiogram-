1581739483e3495bfbbf5b461a044243a0ed6751
vladimir <vovka1998@gmail.com>
2025-03-27 10:22:17 +0300

    added method aupdate

--- todo_list/tasks_todo/serializers.py
+++ todo_list/tasks_todo/serializers.py
@@ -1,4 +1,4 @@
-from adrf.serializers import ModelSerializer, Serializer
+from adrf.serializers import ModelSerializer
 from asgiref.sync import sync_to_async
 from rest_framework import serializers
 
@@ -33,3 +33,35 @@ class TaskSerializer(ModelSerializer):
                 tag, _ = await Tag.objects.aget_or_create(name=tag["name"])
                 await sync_to_async(task.tags.add)(tag)
             return task
+
+    async def aupdate(self, instance: Task, validated_data):
+        tags_data = validated_data.pop("tags")
+        async with AsyncAtomicContextManager():
+            # Обновление полей задачи
+            instance.title = validated_data.get("title")
+            instance.description = validated_data.get("description")
+            instance.date_end = validated_data.get("date_end")
+            await instance.asave()
+
+            # Текущие теги
+            current_tags = {tag.name: tag for tag in instance.tags.all()}
+            requested_tags = set()
+
+            # Обработка элементов
+            for tag_data in tags_data:
+                tag_name = tag_data.get('tag_name')
+
+                # Проверка наличия тега
+                if tag_name and tag_name in current_tags:
+                    requested_tags.add(tag_name)
+
+                # Создание нового тега
+                else:
+                    new_tag, _ = await Tag.objects.aget_or_create(name=tag_data["name"])
+                    await sync_to_async(instance.tags.add)(new_tag)
+
+            # Удаление отсутствующих в запросе элементов
+            for tag_name, tag in current_tags.items():
+                if tag_name not in requested_tags:
+                    await sync_to_async(instance.tags.remove)(tag)
+        return instance
